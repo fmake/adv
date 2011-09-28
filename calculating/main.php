8a2621d085a3b751b8024ce244d4730d5a6bba5e
Nikita <shevlyakov.nikita@gmail.com>
2011-09-28 17:01:12 +0400

    Init commit

---
+++ calculating/main.php
@@ -0,0 +1,3 @@
+<?php
+
+	$modul->template = "base/main.tpl";
---
+++ index.php
@@ -0,0 +1,26 @@
+<?
+header('Content-type: text/html; charset=utf-8'); 
+setlocale(LC_ALL, 'ru_RU.UTF-8');
+mb_internal_encoding('UTF-8');
+
+ini_set('display_errors',1);
+error_reporting(7);
+session_start();
+
+require('./fmake/FController.php');
+
+$modul = new fmakeSiteModule();
+$modul->getPage($request -> getEscape('modul') ,$twig);
+$globalTemplateParam->set('modul',$modul);
+
+//добавляем каталог к основным модулям
+$menu = $modul->getAllForMenu(0, true,$q=false,$flag=true,true);
+$globalTemplateParam->set('menu',$menu);	
+$modul->template = "base/main.tpl";
+if($modul->file){
+	include($modul->file.".php");
+} 
+
+
+$template = $twig->loadTemplate($modul->template);
+$template->display($globalTemplateParam->get());
