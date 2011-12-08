7ba4a25aa84b024a51feefb6bca0d9696f55d30f
n1k <shevlyakov.nikita@gmail.com>
2011-12-08 22:06:29 +0300

    Доделал подсчет денег, тестовая версия, без последовательного подсчета

--- fmake/modules/projects/seo/money.php
+++ fmake/modules/projects/seo/money.php
@@ -2,7 +2,7 @@
 class projects_seo_money extends fmakeCore{
 		
 	public $table = "projects_seo_money";
-	public $idField = "id_query";
+	public $idField = array("id_project","id_seo_search_system","date");
 
 	
 	function checkMoney($date = false){
@@ -30,6 +30,7 @@ class projects_seo_money extends fmakeCore{
 				for ($k = 0; $k < $index3; $k++) {
 					$pos = $seoPosition -> getPositionByQueryDate($projectsSeo[$i]['search_systems'][$j]['querys'][$k][$seoQuery -> idField], $date);
 					for ($h = 0; $h < $index4; $h++) {
+						// проверяем позицию запроса по правилу
 						if( $projectsSeo[$i]['search_systems'][$j]['exs'][$h]['from'] <= $pos['pos']
 								&&	$pos['pos'] <= $projectsSeo[$i]['search_systems'][$j]['exs'][$h]['to'] ){
 							$price = ( $searchSystemExsPrice -> 
@@ -41,10 +42,66 @@ class projects_seo_money extends fmakeCore{
 					}
 				}
 				echo $money;
+				$this -> addParam("money", $money);
+				$this -> addParam("id_project", $projectsSeo[$i][$projects -> idField]);
+				$this -> addParam("id_seo_search_system", $projectsSeo[$i]['search_systems'][$j]['id_seo_search_system']);
+				$this -> addParam("date", $date);
+				$this -> newItem();
 			}
 			exit;
 		}
 		
 	}
+	
+	/**
+	*
+	* Создание нового объекта, с использованием массива params
+	*/
+	function newItem(){
+		if($this -> getMoneyByProjectSearchSystemDate($this -> params['id_project'],$this -> params['id_seo_search_system'],$this -> params['date'])){
+			$this->update();
+			return;
+		}
+		$insert = $this->dataBase->InsertInToDB(__LINE__);
+		$insert	-> addTable($this->table);
+		$this->getFilds();
+		
+		if($this->filds){
+			foreach($this->filds as $fild){
+				if(!isset($this->params[$fild])) continue;
+					$insert -> addFild("`".$fild."`", $this->params[$fild]);
+			}
+		}
+		$insert->queryDB();
+		$this->id = $insert	-> getInsertId();
+	}
+	
+	function update() {
+		
+		if(!$this->filds)
+			$this->getFilds();
+		
+		foreach($this->filds as $fild){
+			if(!isset($this->params[$fild]) || in_array($fild,  $this -> idField)) continue;
+				$update =  $this->dataBase->UpdateDB( __LINE__);
+				$update	-> addTable($this->table) -> addFild("`".$fild."`", $this->params[$fild]);
+				
+				foreach($this->idField as $fld){
+					$update -> addWhere("{$fld}='".$this->params[$fld]."'");
+				}
+			$update  -> queryDB();
+		}
+	}	
+	
+	/**
+	* получить позиции по запросу в промежутке
+	*/
+	function getMoneyByProjectSearchSystemDate( $id_project,$id_seo_search_system, $date){
+		$where[] = "`id_project` = '{$id_project}'";
+		$where[] = "`id_seo_search_system` >= '{$id_seo_search_system}'";
+		$where[] = "`date` = '{$date}'";
+		return ($this -> getWhere($where));
+	}
+	
 	
 }
